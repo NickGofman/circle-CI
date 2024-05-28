7e9f75a037c7829fa19b581af642616f2f0f4b52
nikg1999 <nikg1999@gmail.com>
2024-05-28 15:16:53 +0300

    update tests

--- app/tests/test_server.py
+++ app/tests/test_server.py
@@ -5,7 +5,7 @@ class TestFlaskServer(unittest.TestCase):
     def test_server_is_up(self):
         response = None
         try:
-            response = requests.get('http://127.0.0.1/',verify=False, timeout=5)
+            response = requests.get('http://127.0.0.1:8000',verify=False, timeout=5)
             self.assertEquals(response.status_code, 200, "Server is Down")
         except requests.ConnectionError:
             self.assertIsNotNone(response, "Server is Down")
